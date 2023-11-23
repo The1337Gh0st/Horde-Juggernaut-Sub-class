61356a81ec12f0d14cedd95c1ae6d845f3f582ec
The1337Gh0st <cwarrior1414@gmail.com>
2023-11-23 03:51:15 -0500

    Shockproof + Crowd Control rework
    Replaced Shockproof's barrier regen with passive 50% debuff buildup reduction
    
    Buffed Crowd Control's stun amount to 500 (can now stun Elites), now only regenerates vanilla Horde's shrapnel grenades (gives HL2 frag instead if item is not present in config)

--- lua/horde/gamemode/perks/juggernaut_crowd_control.lua
+++ lua/horde/gamemode/perks/juggernaut_crowd_control.lua
@@ -1,5 +1,5 @@
 PERK.PrintName = "Crowd Control"
-PERK.Description = "Regenerate a special version of Shrapnel Grenade every {1} seconds. \nSpecial Shrapnel Grenades do not take up any weight. \nShrapnel Grenades now deal {2} more damage and inflict Stun."
+PERK.Description = "Regenerate a Shrapnel Grenade every {1} seconds. \nShrapnel Grenades deal {2} more damage and inflict massive Stun."
 PERK.Icon = "materials/perks/frag_impact.png"
 PERK.Params = {
     [1] = {value = 10},
@@ -11,10 +11,17 @@ PERK.Hooks.Horde_OnSetPerk = function(ply, perk)
     if SERVER and perk == "juggernaut_crowd_control" then
         timer.Create("Horde_JuggernautCrowdControl" .. ply:SteamID(), 10, 0, function ()
             if not ply:IsValid() or not ply:Alive() then return end
-				--regenerate fake but similar shrapnel grenades to ensure that it can be used regardless of item config / player weight
-                if not ply:HasWeapon("arccw_horde_shrapnel_backup") then
-                    ply:Give("arccw_horde_shrapnel_backup", ply:GetAmmoCount("arccw_horde_shrapnel_backup") > 0)
+			
+			if HORDE.items["arccw_horde_nade_shrapnel"] then
+                if not ply:HasWeapon("arccw_horde_nade_shrapnel") then
+                    ply:Give("arccw_horde_nade_shrapnel", ply:GetAmmoCount("arccw_horde_nade_shrapnel") > 0)
                 end
+            else
+                -- Don't like m67 for some reason.
+                if not ply:HasWeapon("weapon_frag") then
+                    ply:Give("weapon_frag", ply:GetAmmoCount("Grenade") > 0)
+                end
+            end
             end)
         end
     end
@@ -22,7 +29,6 @@ PERK.Hooks.Horde_OnSetPerk = function(ply, perk)
 PERK.Hooks.Horde_OnUnsetPerk = function(ply, perk)
     if SERVER and perk == "juggernaut_crowd_control" then
         timer.Remove("Horde_JuggernautCrowdControl" .. ply:SteamID())
-		ply:StripWeapon("arccw_horde_shrapnel_backup")
     end
 end
 
@@ -30,7 +36,14 @@ PERK.Hooks.Horde_OnPlayerDamage = function (ply, npc, bonus, hitgroup, dmginfo)
     if not ply:Horde_GetPerk("juggernaut_crowd_control")  then return end
     if dmginfo:GetInflictor():GetClass() == "arccw_thr_shrapnel" then
         bonus.more = bonus.more * 3
-		npc:Horde_AddDebuffBuildup(HORDE.Status_Stun, 200, ply, npc:GetPos())
+		npc:Horde_AddDebuffBuildup(HORDE.Status_Stun, 500, ply, npc:GetPos())
     end
+	--bootleg shrapnel grenade conversion
+	if dmginfo:GetInflictor():GetClass() == "npc_grenade_frag" then
+		dmginfo:SetDamageType(DMG_BULLET)
+        bonus.more = bonus.more * 3
+		npc:Horde_AddDebuffBuildup(HORDE.Status_Stun, 500, ply, npc:GetPos())
+    end
+	
 end
 
